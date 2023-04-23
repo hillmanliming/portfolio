58a0d277b39971c094ad914aaf2cfa47246dcf78
LiMing Hillman <liming@hillmanconsult.com>
2023-04-23 13:41:24 +0200

    sfg

---
+++ assets/script/javascript.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  const name = document.getElementById("loadscreen");
+  name.style.display = "none";
+}, 3500);
+
+setTimeout(() => {
+  const name = document.getElementById("home-main");
+  name.style.display = "block";
+}, 3500);
