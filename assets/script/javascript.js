dd52952e7c99e13ccc51391bdd9e48a0bfa70975
LiMing Hillman <503055@student.fontys.nl>
2024-06-04 16:16:56 +0200

    WORKING CLEAN update text on click

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,34 +1,36 @@
 let parSplash = document.querySelectorAll(".par-splash");
+let parSplashArray = [...parSplash]; // converts NodeList to Array
 
 let school = document
 	.getElementById("school")
 	.addEventListener("click", function () {
-		alert("hello");
-
-		var parSplash = document.querySelectorAll(".par-splash"); // returns NodeList
-		var parSplashArray = [...parSplash]; // converts NodeList to Array
 		parSplashArray.forEach((parSplash) => {
-			parSplash.innerHTML = "hello";
+			parSplash.innerHTML = "SCHOOL";
 		});
 	});
 
-// let piano = document
-// 	.getElementById("piano")
-// 	.addEventListener("click", function () {
-// 		parSplash.innerHTML = "hello";
-// 	});
+let piano = document
+	.getElementById("piano")
+	.addEventListener("click", function () {
+		parSplashArray.forEach((parSplash) => {
+			parSplash.innerHTML = "PIANO";
+		});
+	});
 
-// let about = document
-// 	.getElementById("piano")
-// 	.addEventListener("click", function () {
-// 		parSplash.innerHTML = "hello";
-// 	});
+let about = document
+	.getElementById("about")
+	.addEventListener("click", function () {
+		parSplashArray.forEach((parSplash) => {
+			parSplash.innerHTML = "ABOUT";
+		});
+	});
 
-// let contact = document
-// 	.getElementById("contact")
-// 	.addEventListener("click", function () {
-// 		alert("hello");
-// 		parSplash.textContent = "hello";
-// 	});
+let contact = document
+	.getElementById("contact")
+	.addEventListener("click", function () {
+		parSplashArray.forEach((parSplash) => {
+			parSplash.innerHTML = "CONTACT";
+		});
+	});
 
 // If clicked on wxyz elemnt from header, change p1/p2 to correlating header name hard coded.
